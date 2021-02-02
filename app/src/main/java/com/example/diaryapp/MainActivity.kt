361a0ae9e1dec17f1cadca49fb3cb27716f8a659
soojik <wltn2858@swu.ac.kr>
2021-02-02 22:28:19 +0900

    Add layout(main, register)

--- app/src/main/java/com/example/diaryapp/MainActivity.kt
+++ app/src/main/java/com/example/diaryapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.diaryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btn_signUp = findViewById<Button>(R.id.btn_signUp)
+
+        btn_signUp.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
