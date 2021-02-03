b395cad5f169549e905da43f429cbfc8281dc74a
soyoung12 <hena019@naver.com>
2021-02-03 18:22:08 +0900

    날짜 선택시 일기 쓰는 창으로 바뀌고 날짜 반영

--- app/src/main/java/com/example/diaryapp/DiaryActivity.kt
+++ app/src/main/java/com/example/diaryapp/DiaryActivity.kt
@@ -6,18 +6,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import android.widget.CalendarView
 
 class DiaryActivity : AppCompatActivity() {
     lateinit var auth : FirebaseAuth
     lateinit var btn_logout : Button
+    lateinit var calendarView: CalendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
 
         auth = FirebaseAuth.getInstance()
-
         btn_logout = findViewById(R.id.btn_logout)
+        calendarView=findViewById<CalendarView>(R.id.calendarView)
 
         btn_logout.setOnClickListener {
             Toast.makeText(baseContext, "로그아웃", Toast.LENGTH_SHORT).show()
@@ -25,5 +27,18 @@ class DiaryActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+
+        calendarView.setOnDateChangeListener {  view, year, month, day->
+
+            var intent = Intent(this, QuestionDiaryActivity::class.java)
+            intent.putExtra("year",year.toString())
+            intent.putExtra("month",month.toString()+1)
+            intent.putExtra("day",day.toString())
+            startActivity(intent)
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/diaryapp/QuestionDiaryActivity.kt
+++ app/src/main/java/com/example/diaryapp/QuestionDiaryActivity.kt
@@ -2,10 +2,23 @@ package com.example.diaryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class QuestionDiaryActivity : AppCompatActivity() {
+    lateinit var monthTextView:TextView
+    lateinit var dayTextView:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_diary)
+
+        monthTextView = findViewById<TextView>(R.id.monthTextView)
+        dayTextView = findViewById<TextView>(R.id.monthTextView)
+        var year = intent.getStringExtra("year")+"년"
+        var month = intent.getStringExtra("month")+"월"
+        var day = intent.getStringExtra("day")+"일"
+        monthTextView.setText(month)
+        dayTextView.setText(day)
     }
 }
