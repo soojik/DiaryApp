374447410f355e418b15c3bbb044e345a8a6a12c
ysy <downtime8454@naver.com>
2021-02-03 15:33:28 +0900

    Question_diary_layout

---
+++ app/src/main/java/com/example/diaryapp/FeelingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.diaryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeelingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feeling)
+    }
+}
