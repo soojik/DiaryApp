74e0fd2f279a522620e748e12da6f30164526004
soyoung12 <hena019@naver.com>
2021-02-03 18:53:39 +0900

    날짜 선택시 일기 쓰는 창으로 바뀌고 날짜 반영( 오류 수정)

--- app/src/main/java/com/example/diaryapp/QuestionDiaryActivity.kt
+++ app/src/main/java/com/example/diaryapp/QuestionDiaryActivity.kt
@@ -7,18 +7,24 @@ import android.widget.TextView
 class QuestionDiaryActivity : AppCompatActivity() {
     lateinit var monthTextView:TextView
     lateinit var dayTextView:TextView
-
+    lateinit var yearTextView:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_diary)
 
         monthTextView = findViewById<TextView>(R.id.monthTextView)
-        dayTextView = findViewById<TextView>(R.id.monthTextView)
+        dayTextView = findViewById<TextView>(R.id.dayTextView)
+        yearTextView = findViewById<TextView>(R.id.yearTextView)
         var year = intent.getStringExtra("year")+"년"
         var month = intent.getStringExtra("month")+"월"
         var day = intent.getStringExtra("day")+"일"
+        yearTextView.setText(year)
         monthTextView.setText(month)
         dayTextView.setText(day)
+
+
+
+
     }
 }
