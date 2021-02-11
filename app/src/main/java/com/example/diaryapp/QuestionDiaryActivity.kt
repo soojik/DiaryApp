25023bf23a4c4f6fd9c303678208e7ed956a8838
KristaYJ <krista306@naver.com>
2021-02-11 12:48:21 +0900

    cam, rec

--- app/src/main/java/com/example/diaryapp/QuestionDiaryActivity.kt
+++ app/src/main/java/com/example/diaryapp/QuestionDiaryActivity.kt
@@ -1,14 +1,34 @@
 package com.example.diaryapp
 
+import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.AudioManager
+import android.media.MediaParser
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.Handler
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
 import android.widget.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class QuestionDiaryActivity : AppCompatActivity() {
 
@@ -22,9 +42,38 @@ class QuestionDiaryActivity : AppCompatActivity() {
     lateinit var questionTextView : TextView
     lateinit var answerEditText : EditText
     lateinit var answerBtn : Button
-    lateinit var  diarySaveBtn:Button
+    lateinit var diarySaveBtn:Button
     lateinit var weatherView : ImageView
     lateinit var feelingImgView : ImageView
+    lateinit var imgUploadBtn : Button
+    lateinit var cameraBtn : Button
+    lateinit var galleryBtn : Button
+    lateinit var imageImgView : ImageView
+    lateinit var recordBtn : Button
+    lateinit var recStartBtn : Button
+    lateinit var recStopBtn : Button
+    lateinit var recHearStartBtn : ImageButton
+    lateinit var recHearStopBtn : ImageButton
+    lateinit var seekbar : SeekBar
+    lateinit var recHearSoundBtn : ImageView
+    lateinit var mr : MediaRecorder
+    lateinit var mp : MediaPlayer
+    lateinit var handler: Handler
+
+    val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+    val STORAGE_PERMISSION = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE
+        , Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    val FLAG_PERM_CAMERA = 98
+    val FLAG_PERM_STORAGE = 99
+    val FLAG_REQ_CAMERA = 101
+    val FLAG_REQ_STORAGE = 102
+
+    private lateinit var startTime: TextView
+    private lateinit var songTime: TextView
+    private var onTime: Int = 0
+    private var playTime: Int = 0
+    private var endTime: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +91,24 @@ class QuestionDiaryActivity : AppCompatActivity() {
         diarySaveBtn = findViewById(R.id.diarySaveBtn)
         weatherView = findViewById(R.id.weatherView)
         feelingImgView = findViewById(R.id.feelingImgView)
+        imgUploadBtn = findViewById(R.id.imgUploadBtn)
+        cameraBtn = findViewById(R.id.cameraBtn)
+        galleryBtn = findViewById(R.id.galleryBtn)
+        imageImgView = findViewById(R.id.imageImgView)
+        recordBtn = findViewById(R.id.recordBtn)
+        recStartBtn = findViewById(R.id.recStartBtn)
+        recStopBtn = findViewById(R.id.recStopBtn)
+        recHearStartBtn = findViewById(R.id.recHearStartBtn)
+        recHearStopBtn = findViewById(R.id.recHearStopBtn)
+        seekbar = findViewById(R.id.seekBar)
+        recHearSoundBtn = findViewById(R.id.recHearSoundBtn)
+        mr = MediaRecorder()
+        mp = MediaPlayer.create(this, R.raw.myrec)
+        seekbar.isClickable = false
+        seekbar.isEnabled = true
 
+        startTime = findViewById(R.id.txtStartTime)
+        songTime = findViewById(R.id.txtSongTime)
 
         var year = intent.getStringExtra("year").toString()
         var monthString = intent.getStringExtra("month").toString()
@@ -73,9 +139,47 @@ class QuestionDiaryActivity : AppCompatActivity() {
             startActivityForResult(intent,2)
             Toast.makeText(this, "엑티비티 변환", Toast.LENGTH_SHORT).show()
         }
+
+        if (checkPermission(STORAGE_PERMISSION, FLAG_PERM_STORAGE)) {
+            setViews()
+        }
+
+        imgUploadBtn.setOnClickListener {
+            imgUploadBtn.visibility = View.GONE
+            cameraBtn.visibility = View.VISIBLE
+            galleryBtn.visibility = View.VISIBLE
+            imageImgView.visibility = View.VISIBLE
+        }
+
+
+        recordBtn.setOnClickListener {
+            recordBtn.visibility = View.GONE
+            recStartBtn.visibility = View.VISIBLE
+        }
+
+        recStartBtn.isEnabled = false
+        recStopBtn.isEnabled = false
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE), 111)
+            recStartBtn.isEnabled = true
+
+        recStartBtn.setOnClickListener {
+            startRecording()
+        }
+        recStopBtn.setOnClickListener {
+            stopRecording()
+        }
+
+        recHearStartBtn.setOnClickListener {
+            playRecord()
+        }
+
+        recHearStopBtn.setOnClickListener {
+            stopRecord()
+        }
     }
-
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -101,6 +205,24 @@ class QuestionDiaryActivity : AppCompatActivity() {
                 }
             }
         }
+
+        if(resultCode == Activity.RESULT_OK) {
+            when(requestCode){
+                FLAG_REQ_CAMERA -> {
+                    if (data?.extras?.get("data") != null) {
+                        val bitmap = data?.extras?.get("data") as Bitmap
+//                        imagePreview.setImageBitmap(bitmap)
+                        val uri = saveImageFile(newFileName(), "image/jpg", bitmap)
+                        imageImgView.setImageURI(uri)
+                    }
+                }
+                FLAG_REQ_STORAGE -> {
+                    val uri = data?.data
+                    imageImgView.setImageURI(uri)
+                }
+            }
+        }
+
     }
 
     //선택한 기분 또는 날씨 저장
@@ -227,6 +349,108 @@ class QuestionDiaryActivity : AppCompatActivity() {
         }
     }
 
+    // 이미지 업로드
+    fun setViews(){ // 이미지 업로드 함수
+        cameraBtn.setOnClickListener {
+            openCamera()
+        }
+        galleryBtn.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    fun openCamera() { // 카메라 여는 함수
+        if (checkPermission(CAMERA_PERMISSION, FLAG_PERM_CAMERA)) {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, FLAG_REQ_CAMERA)
+        }
+    }
+
+    fun openGallery() { // 갤러리 여는 함수
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = MediaStore.Images.Media.CONTENT_TYPE
+        startActivityForResult(intent, FLAG_REQ_STORAGE)
+    }
+
+
+    fun saveImageFile(filename:String, mimeType:String, bitmap: Bitmap) : Uri? { // 사진 저장 함수
+        var values = ContentValues()
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+        val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        try {
+            if(uri != null) {
+                var descriptor = contentResolver.openFileDescriptor(uri, "w")
+                if(descriptor != null) {
+                    val fos = FileOutputStream(descriptor.fileDescriptor)
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+                    fos.close()
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        values.clear()
+                        values.put(MediaStore.Images.Media.IS_PENDING, 0)
+                        contentResolver.update(uri, values, null, null)
+                    }
+                }
+            }
+        }catch (e:java.lang.Exception) {
+            Log.e("File", "error=${e.localizedMessage}")
+        }
+        return uri
+    }
+
+    fun newFileName() : String {
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = sdf.format(System.currentTimeMillis())
+
+        return "$filename.jpg"
+    }
+
+    fun checkPermission(permissions: Array<out String>, flag:Int) : Boolean { // 사진 권한 처리
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (permission in permissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, permissions, flag)
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) { // 사진 권한 처리
+        when (requestCode) {
+            FLAG_PERM_STORAGE -> {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "저장소 권한 필요", Toast.LENGTH_LONG).show()
+                        finish()
+                        return
+                    }
+                }
+                setViews()
+            }
+            FLAG_PERM_CAMERA -> {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "카메라 권한 승인 필요", Toast.LENGTH_LONG).show()
+                        return
+                    }
+                }
+                openCamera()
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==111 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            recStartBtn.isEnabled = true
+    }
+
     // 일기
     fun readDiary(cYear : String, cMonth : String, cDay : String){ //일기
         var fName = "" + cYear + cMonth + cDay + "_" + userName + "_" + "Diary" + ".txt" //일기 파일 이름
@@ -251,4 +475,62 @@ class QuestionDiaryActivity : AppCompatActivity() {
         }
 
     }
+
+
+    // 음성녹음
+    fun startRecording(){
+        mr.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mr.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mr.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
+        var path = Environment.getExternalStorageDirectory().toString()+"/myrec.mp3"
+        mr.setOutputFile(path)
+        mr.prepare()
+        mr.start()
+        Toast.makeText(this, "녹음 시작", Toast.LENGTH_SHORT).show()
+        recStopBtn.isEnabled = true
+        recStartBtn.isEnabled = false
+        recStartBtn.visibility = View.GONE
+        recStopBtn.visibility = View.VISIBLE
+    }
+
+    fun stopRecording(){
+        mr.stop()
+        recStopBtn.visibility = View.GONE
+        recStartBtn.visibility = View.VISIBLE
+        recHearStartBtn.visibility = View.VISIBLE
+        recHearStopBtn.visibility = View.VISIBLE
+        seekbar.visibility = View.VISIBLE
+        recHearSoundBtn.visibility = View.VISIBLE
+        recStartBtn.isEnabled = true
+        recStopBtn.isEnabled = false
+    }
+
+    fun playRecord(){
+        Toast.makeText(this, "음성 재생", Toast.LENGTH_SHORT).show()
+        mp.start()
+        endTime = mp.duration
+        playTime = mp.currentPosition
+        seekbar.max = endTime
+        onTime = 1
+        seekbar.progress = playTime
+        handler.postDelayed(updateSongTime, 100)
+        recHearStopBtn.isEnabled = true
+        recHearStartBtn.isEnabled = false
+    }
+
+    fun stopRecord() {
+        mp.pause()
+        recHearStopBtn.isEnabled = false
+        recHearStartBtn.isEnabled = true
+        Toast.makeText(applicationContext, "음성 멈춤", Toast.LENGTH_SHORT).show()
+    }
+
+    private val updateSongTime = object : Runnable {
+        override fun run() {
+            playTime = mp.currentPosition
+                        seekbar.progress = playTime
+                        handler.postDelayed(this, 100)
+        }
+    }
+
 }
